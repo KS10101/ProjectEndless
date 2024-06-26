19392bbcf2502c5514bae125097dbaed22074178
KS10101 <kunal.singh@kompanions.in>
2024-06-26 16:01:01 +0530

    Leaderboard Feature update
    - Instantiate the leaderboard entry from the data saved in the form of json

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -10,4 +11,33 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button ExitButton;
 
     [SerializeField] private GameObject LeaderboardPanel;
+
+    private void OnEnable()
+    {
+        PlayButton.onClick.AddListener(PlayGame);
+        LeaderboardButton.onClick.AddListener(BuildLeaderboard);
+        ExitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void OnDisable()
+    {
+        PlayButton.onClick.RemoveListener(PlayGame);
+        LeaderboardButton.onClick.RemoveListener(BuildLeaderboard);
+        ExitButton.onClick.RemoveListener(ExitGame);
+    }
+
+    private void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void BuildLeaderboard()
+    {
+        LeaderboardPanel.SetActive(true);
+    }
+
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
 }
