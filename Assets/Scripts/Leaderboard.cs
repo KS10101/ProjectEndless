e57baf98c4416a2b3e15defa96209c8a682df958
KS10101 <kunal.singh@kompanions.in>
2024-06-28 17:15:52 +0530

    Update Leaderboard.cs

--- Assets/Scripts/Leaderboard.cs
+++ Assets/Scripts/Leaderboard.cs
@@ -1,7 +1,10 @@
 using Dreamteck;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class Leaderboard : MonoBehaviour
@@ -19,12 +22,13 @@ public class Leaderboard : MonoBehaviour
 
         UpdateList();
         AddScoreCard("al-chan", 19);
-        AddScoreCard("Gamora-Kun", 2);
-        AddScoreCard("Divyanshu-Sama", 100);
-        AddScoreCard("Vidhayak ji", 500);
-        AddScoreCard("Sachiv ji", 200);
-        AddScoreCard("Kartik cha", 5);
+        AddScoreCard("Gamora-Kun", 200);
+        AddScoreCard("Divyanshu-Sama", 200);
+        AddScoreCard("Vidhayak ji", 200);
+        AddScoreCard("Dhruv-San", 200);
+        AddScoreCard("Kartik cha", 200);
         AddScoreCard("Mishra ji", 1000);
+        AddScoreCard("Cat", 200);
     }
 
     public void UpdateList()
@@ -41,19 +45,67 @@ public class Leaderboard : MonoBehaviour
     public void CreateEntry()
     {
         UpdateList();
-        var entries = _ScoreCardEntries.OrderByDescending(s => s.Score);
-        ScoreEntriesArray = entries.ToArray();
-        
+        ScoreEntriesArray = SortList(_ScoreCardEntries);
+
+        if (ScoreEntriesArray == null) return;
         // if score card entries is not null 
+        Debug.Log($"Array Size: {ScoreEntriesArray.Count()}");
         for (int i = 0; i < ScoreEntriesArray.Count(); i++)
         {
             GameObject entry = Instantiate(EntryPrefab, ScoreCardsContainer);
             entry.SetActive(true);
             entry.GetComponent<ScoreEntriesSetter>().SetDataEntries(i+1, ScoreEntriesArray[i].Name, ScoreEntriesArray[i].Score);
-
+            
         }
+
+
     }
 
+    //public bool CheckforHighScore(int Score)
+    //{
+    //    bool CheckStatus = false;
+    //    UpdateList();
+    //    if (_ScoreCardEntries.Count < 8)
+    //        CheckStatus = true;
+    //    else if (_ScoreCardEntries.Count == 8 && Score > GetLowestScore())
+    //    {
+    //        CheckStatus = true;
+    //    }
+
+    //    return CheckStatus;
+    //}
+
+    public int GetEntryCount()
+    {
+        UpdateList();
+
+        return _ScoreCardEntries.Count;
+    }
+
+    public int GetHighestScore()
+    {
+        UpdateList();
+        ScoreCard[] entries = SortList(_ScoreCardEntries);
+
+        return entries[0].Score;
+    }
+
+    public int GetLowestScore()
+    {
+        UpdateList();
+        ScoreCard[] entries = SortList(_ScoreCardEntries);
+
+        return entries[entries.Count() - 1].Score;
+    }
+
+    public ScoreCard[] SortList(List<ScoreCard> Scorelist)
+    {
+        if (Scorelist == null || Scorelist.Count == 0) return null;
+
+        var entries = Scorelist.OrderByDescending(s => s.Score);
+
+        return entries.ToArray();
+    }
 
     public void AddScoreCard(ScoreCard _scoreCard)
     {
