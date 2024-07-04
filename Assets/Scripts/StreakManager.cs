85d2d654453fe3006f961e288a2e6e585deb5585
KS10101 <kunal.singh@kompanions.in>
2024-07-04 15:15:37 +0530

    added streak animation

--- Assets/Scripts/StreakManager.cs
+++ Assets/Scripts/StreakManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class StreakManager : MonoBehaviour
@@ -9,12 +10,16 @@ public class StreakManager : MonoBehaviour
     private int streak = 0;
     [SerializeField] private int scoreStreak = 0;
     [SerializeField] private int desiredStreak = 3;
-    [SerializeField] private int multiplier = 1; 
+    [SerializeField] private int multiplier = 1;
+    [SerializeField] private Animator streakAnimator;
+    [SerializeField] private TextMeshProUGUI streakNum;
 
     private void Awake()
     {
         if (instance == null)
             instance = this;
+
+        streakAnimator.enabled = false;
     }
 
     public void MakeHealthStreak()
@@ -30,12 +35,16 @@ public class StreakManager : MonoBehaviour
 
     public void MakeScoreStreak()
     {
+        streakAnimator.StopPlayback();
         scoreStreak++;
         if (scoreStreak >= desiredStreak)
         {
             multiplier ++;
             ScoreManager.instance.scoreMultiplier = multiplier;
-
+            streakAnimator.gameObject.SetActive(true);
+            streakNum.text = multiplier.ToString();
+            streakAnimator.enabled = true;
+            streakAnimator.SetTrigger("streak");
         }
     }
 
@@ -53,6 +62,8 @@ public class StreakManager : MonoBehaviour
         {
             scoreStreak = 0;
             multiplier = 1;
+            streakAnimator.enabled = false;
+            streakAnimator.gameObject.SetActive(false);
         }
     }
 
